ddee0be5550ab9c7a3cf257439b07640bb071038
Andy Yu <andy.yu@autogeneral.com.au>
2021-12-18 14:40:37 +1000

    - populated data - collapsing/expanding broker appointments. - show appointment details on selection.

--- app/src/components/Root/AppointmentSelect/AppointmentSelect.tsx
+++ app/src/components/Root/AppointmentSelect/AppointmentSelect.tsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 
 import Broker from "./Broker";
@@ -16,34 +17,93 @@ const Heading = styled.strong.attrs({ role: "heading", level: 2 })`
   font-size: 20px;
 `;
 
-type BrokerAppointments = {
+export interface Broker {
   id: number;
   name: string;
-  appointments: { id: number; brokerId: number; date: string }[];
-}[];
+}
+
+export interface Appointment {
+  id: number;
+  brokerId: number;
+  date: string;
+}
+
+export interface BrokerAppointment extends Broker {
+  appointments: Appointment[];
+}
+
+interface SelectedBrokerAppointment extends Broker {
+  appointment: Appointment | undefined;
+}
 
 const AppointmentSelect = () => {
-  axios
-    .get("http://localhost:8080/brokers")
-    .then(({ data }) => console.log(data));
-  axios
-    .get("http://localhost:8080/appointments")
-    .then(({ data }) => console.log(data));
+  const [brokerAppointments, setBrokerAppointments] = useState<
+    BrokerAppointment[]
+  >([]);
+  const [selectedBrokerAppointment, setSelectedBrokerAppointment] =
+    useState<SelectedBrokerAppointment>();
+
+  const onSelect = (brokerId: number, appointmentId: number) => {
+    const selectedBroker = brokerAppointments.find(
+      (brokerAppointment: BrokerAppointment) =>
+        brokerAppointment.id === brokerId
+    );
+
+    const selectedAppointment = selectedBroker?.appointments.find(
+      (appointment: Appointment) => appointment.id === appointmentId
+    );
+
+    if (selectedBroker) {
+      setSelectedBrokerAppointment({
+        id: selectedBroker.id,
+        name: selectedBroker.name,
+        appointment: selectedAppointment,
+      });
+    }
+  };
+
+  useEffect(() => {
+    Promise.all([
+      axios.get("http://localhost:8080/brokers"),
+      axios.get("http://localhost:8080/appointments"),
+    ]).then(([brokersData, appointmentsData]) => {
+      if (brokersData?.data?.length > 0) {
+        const brokerAppointmentsData: BrokerAppointment[] =
+          brokersData.data.map((broker: Broker) => ({
+            id: broker.id,
+            name: broker.name,
+            appointments:
+              appointmentsData?.data?.length > 0 &&
+              appointmentsData.data.filter(
+                (appointment: Appointment) => appointment.brokerId === broker.id
+              ),
+          }));
+        setBrokerAppointments(brokerAppointmentsData);
+      }
+    });
+  }, []);
 
   return (
     <Wrapper>
       <SideBar>
         <Heading>Amazing site</Heading>
-        TODO: populate brokers
         <ul>
-          {/* {brokerAppointments.map((broker) => (
-            <Broker key={broker.id} broker={broker} />
-          ))} */}
+          {brokerAppointments.length > 0 &&
+            brokerAppointments.map((broker: BrokerAppointment) => (
+              <Broker key={broker.id} broker={broker} onSelect={onSelect} />
+            ))}
         </ul>
       </SideBar>
       <div>
         <Heading>Appointment details</Heading>
-        TODO: get appointment details when clicking on one from the left side
+        {selectedBrokerAppointment && (
+          <>
+            <p>Broker's name: {selectedBrokerAppointment.name}</p>
+            <p>
+              Appointment Date: {selectedBrokerAppointment?.appointment?.date}
+            </p>
+          </>
+        )}
       </div>
     </Wrapper>
   );
--- app/src/components/Root/AppointmentSelect/Broker/Broker.tsx
+++ app/src/components/Root/AppointmentSelect/Broker/Broker.tsx
@@ -1,23 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import { Appointment, BrokerAppointment } from "../AppointmentSelect";
 
 export interface BrokerProps {
-  broker: {
-    name: string;
-    id: number;
-    appointments: { id: number; brokerId: number; date: string }[];
-  };
+  broker: BrokerAppointment;
+  onSelect: (arg0: number, arg1: number) => void
 }
 
-const Broker = (broker: BrokerProps) => {
+const Broker = ({ broker, onSelect }: BrokerProps) => {
+  const [isShow, setIsShow] = useState<boolean>(false);
+  const toggle = () => setIsShow(!isShow);
+
   return (
     <li>
-      [broker name]
+      {broker.name}
       <br />
-      appointments:
-      <button>Hide appointments</button>
-      <ul>
-        <li>[appointment date]</li>
-      </ul>
+      {broker.appointments.length > 0 && (
+        <>
+          appointments:
+          <button onClick={toggle}>
+            {isShow ? "Hide" : "Show"} appointments
+          </button>
+          {isShow && (
+            <ul>
+              {broker.appointments.length > 0 &&
+                broker.appointments.map((appointment: Appointment) => (
+                  <li onClick={() => onSelect(broker.id, appointment.id)} key={appointment.id}>{appointment.date}</li>
+                ))}
+            </ul>
+          )}
+        </>
+      )}
     </li>
   );
 };
